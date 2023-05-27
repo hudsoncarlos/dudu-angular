0fda15bf7fb6c69a9c070d80a857307c8adbb9d4
Hudson da Silva Carlos <hudsonscarlos@outlook.com>
2023-05-27 19:10:33 -0300

    feat: Criado componente de cabeçalho da aplicação DuduMemo.

---
+++ dudumemo/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CabecalhoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
