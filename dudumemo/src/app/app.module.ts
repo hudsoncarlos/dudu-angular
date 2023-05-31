af4f905367af26133e4cd7f82201c5a1635cb08e
Hudson da Silva Carlos <hudsonscarlos@outlook.com>
2023-05-30 22:23:27 -0300

    feat: Criação de dois novos componentes que são rodapé e pensamento.

--- dudumemo/src/app/app.module.ts
+++ dudumemo/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent
   ],
   imports: [
     BrowserModule,
